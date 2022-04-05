e142e72730af3f7be0f1415f03fa65bdf6a088df
Erick Chiappone <erick_braga_11@hotmail.com>
2022-04-05 02:01:15 -0300

    commit lista1 e lista3 C#

---
+++ Lista1CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lista1C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
