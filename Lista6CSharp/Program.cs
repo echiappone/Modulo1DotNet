e0229aa18f22e9bdb4667ee1ff1bc810670c984b
Erick Chiappone <erick_braga_11@hotmail.com>
2022-04-06 22:53:01 -0300

    tabuada

---
+++ Lista6CSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lista6CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o numero da tabuada que deseja aprender: ");
+            int tabuada = Int32.Parse(Console.ReadLine());
+
+            for (int i=0; i<=10; ++i)
+            {
+                int resultado = tabuada * i; 
+                Console.WriteLine($" {tabuada} x {i} = {resultado}\n");
+            }
+            Console.WriteLine($"Essa foi a tabuada do {tabuada}");
+        }
+    }
+}
